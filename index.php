346b3aa6b7bb6b410daeb29980447021248cd188
joshuachua <joshua.chua.rose@gmail.com>
2019-10-26 09:16:05 +0800

    menu_order

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>M E N U</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+  </head>
+  <body>
+
+    <form class="" method="POST">
+      <div class="container">
+        <table class="row">
+          <div class="form-group col-4">
+          <h5>Full Name:<input type="text" name="name" value="" placeholder="Fullname" required>
+        </div>
+        <div class="button">
+          <button class="btn btn-lg btn-primary btn-block" type="submit" name="Go">GO</button>
+        </div>>
+        </table>
+      </div>
+    </form>
+
+  </body>
+</html>
