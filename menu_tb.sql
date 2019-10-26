3deab33f9795de28dd0248be4ef68404c6d0588f
joshuachua <joshua.chua.rose@gmail.com>
2019-10-26 09:17:35 +0800

    menu_order

---
+++ menu_tb.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 26, 2019 at 01:16 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `menu`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_tb`
+--
+
+DROP TABLE IF EXISTS `menu_tb`;
+CREATE TABLE IF NOT EXISTS `menu_tb` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fullname` varchar(50) COLLATE armscii8_bin NOT NULL,
+  `menu` varchar(250) COLLATE armscii8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
