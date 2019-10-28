d0c7d54e881fad702cae12aab6ec4b405c2cb573
joshuachua <joshua.chua.rose@gmail.com>
2019-10-28 14:43:55 +0800

    chua

--- resource/php/function/view.php
+++ resource/php/function/view.php
@@ -1,8 +1,5 @@
 <?php
 require_once 'resource/php/class/input.php';
-
 $view= new view();
-
-
 $view->viewAll();
  ?>
---
+++ start.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MENU</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+</head>
+
+<body>
+    <div class="container-flui w-100">
+      <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="d-block" src="resource/img/menu.jpg" width=100% height=695 alt="First slide">
+            <div class="container">
+            <div class="carousel-caption d-none d-sm-block text-center mb-5">
+              <a href="index.php" class="btn btn-outline-dark btn-lg">Start</a>
+            </div>
+          </div>
+          </div>
+          <div   class="carousel-item">
+            <img class="d-block" src="resource/img/blacksmith.jpg"width=100% height=695 alt="Second slide">
+            <div class="container">
+            <div class="carousel-caption d-none d-sm-block text-center mb-5">
+              <a href="index.php" class="btn btn-outline-dark btn-lg">Start</a>
+            </div>
+          </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block" src="resource/img/adobo.jpg" width=100% height=695 alt="Third slide">
+            <div class="container">
+            <div class="carousel-caption d-none d-sm-block text-center mb-5">
+              <a href="index.php" class="btn btn-outline-dark btn-lg">Start</a>
+            </div>
+          </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block" src="resource/img/sinigang.jpg" width=100% height=695 alt="fourth slide">
+            <div class="container">
+            <div class="carousel-caption d-none d-sm-block text-center mb-5">
+              <a href="index.php" class="btn btn-outline-dark btn-lg">Start</a>
+            </div>
+          </div>
+          </div>
+          <div class="carousel-item">
+            <img class="d-block" src="resource/img/tilapia.jpg" width=100% height=695 alt="fifth slide">
+            <div class="container">
+            <div class="carousel-caption d-none d-sm-block text-center mb-5">
+              <a href="index.php" class="btn btn-outline-dark btn-lg">Start</a>
+            </div>
+          </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+
+      </div>
+  </div>
+
+
+
+  </body>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</html>
