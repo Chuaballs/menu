540a8bb9ce10b7d7a06ef8775e4fa9613e3e18bb
joshuachua <joshua.chua.rose@gmail.com>
2019-10-28 18:16:50 +0800

    chua

--- resource/php/function/view.php
+++ resource/php/function/view.php
@@ -20,14 +20,23 @@ public function viewAllData(){
   $pdo->execute();
   $results = $pdo->fetchAll(PDO::FETCH_OBJ);
   echo '<table class="table table-hover text-black  " style="background-color:white;">';
-echo '<thead>';
-echo '<th>#</th> <th>MENU</th> <th>PRICE</th> <th>PICTURE</th> <th>Action</th>';
-echo '</thead>';
+  echo '<thead>';
+  echo '<th>#</th> <th>MENU</th> <th>PRICE</th> <th>PICTURE</th> <th>Action</th>';
+  echo '</thead>';
 foreach ($results as $result) {
     echo '<tr>';
     echo  '<td>'.$result->id.'</td>';
     echo  '<td>'.$result->menu.'</td>';
     echo  '<td>'.$result->price.'</td>';
+    if ($result->menu == 'ADOBO') {
+    echo '<td><img src="resource/img/adobo.jpg" width=100px; /> </td>';
+  }elseif ($result->menu == 'TILAPIA') {
+    echo '<td><img src="resource/img/tilapia.jpg" width=100px; /> </td>';
+  }elseif ($result->menu == 'SISIG') {
+    echo '<td><img src="resource/img/sisig.jpg" width=100px; /> </td>';
+  }elseif ($result->menu == 'CHICKEN JOY'){
+    echo '<td><img src="resource/img/jobiii.jpg" width=100px; /> </td>';
+  }
     echo  '<td><a href="edit.php?id='.$result->id.'" class="btn btn-warning ml-4">Edit</a><a href="delete.php?id='.$result->id.'" class="btn btn-danger ml-4">DELETE</a></td>';
     echo '</tr>';
 }
